f275b5989150d85ab192618815aa8463e5485641
SriramSundaramoorthy <sriram.sundaramoorthy@gmail.com>
2023-10-08 22:21:18 +0530

    Adding Assignment for 6-Oct-2023

---
+++ 6-Oct-2023/assignment_Multiplication_table.py
@@ -0,0 +1,13 @@
+#Task - Get the user input and print the multiplication table upto 10
+multiplier=int(input("Enter the value for the multiplication table : "))
+
+print(f"{multiplier} X 1 = {multiplier*1}")
+print(f"{multiplier} X 2 = {multiplier*2}")
+print(f"{multiplier} X 3 = {multiplier*3}")
+print(f"{multiplier} X 4 = {multiplier*4}")
+print(f"{multiplier} X 5 = {multiplier*5}")
+print(f"{multiplier} X 6 = {multiplier*6}")
+print(f"{multiplier} X 7 = {multiplier*7}")
+print(f"{multiplier} X 8 = {multiplier*8}")
+print(f"{multiplier} X 9 = {multiplier*9}")
+print(f"{multiplier} X 10 = {multiplier*10}")
